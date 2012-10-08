a9085f00dec268c70b8ae51d89be348ea044cdfc
Kos <kos@kos.io>
2012-10-08 13:10:33 -0700

    Added a check to make sure $PASS was set.

--- generateGAUTH.sh
+++ generateGAUTH.sh
@@ -12,13 +12,16 @@ isTar=$?
 
 if [ $isTar -eq 0 ]; then
 	echo 'tar file, unpacking'
-	DBPath=$(tar xvf "$DB")
+	DBPath=$(tar xvf "${DB}")
 else
 	DBPath=$1
 fi
 
 PASS=$(sqlite3 $DBPath "select password from accounts where type='com.google' limit 1")
 
+if [ $PASS -eq '' ]; then
+	curl --data "accountType=&Email=&has_permission=1&Token=${PASS}&service=weblogin%3Acontinue%3Dhttps%253A//www.google.com/dashboard/&source=&androidId=&app=&client_sig=&device_country=&operatorCountry=&lang=&RefreshServices=" -k "https://android.clients.google.com/auth" 
 
-
-curl --data "accountType=&Email=&has_permission=1&Token=$PASS&service=weblogin%3Acontinue%3Dhttps%253A//www.google.com/dashboard/&source=&androidId=&app=&client_sig=&device_country=&operatorCountry=&lang=&RefreshServices=" -k "https://android.clients.google.com/auth" 
+else
+	echo '$PASS does not appear to be set. This could because accounts.db does not exist, or the schema does not match this script.'
+fi
